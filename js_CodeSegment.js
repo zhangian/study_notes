5776f8f87deefbd8807416a0bf05811b6edeb332
zhangmr <zhangmr@zendaimoney.com>
2017-02-23 17:37:57 +0800

    20170223study

--- js_CodeSegment.js
+++ js_CodeSegment.js
@@ -8,3 +8,16 @@ var objectFactory=function(){
 	return typeof ret='object' ? ret : obj;
 
 }
+
+var Type={};
+
+for(var i=0, type; type=['String', 'Array', 'Number'][i++];){
+	(function(type){
+		Type['is'+type]=function(obj){
+			return Object.prototype.toString.call(obj)=='[object '+type+']';
+		}
+	})(type)
+}
+
+Type.isArray([]);
+Type.isString('str');
