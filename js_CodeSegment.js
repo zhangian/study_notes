91adbd81fa5d6e68cb794f5e2909c3b58b15695e
zhangmr <zhangmr@zendaimoney.com>
2017-03-07 16:29:41 +0800

    修改代码片段

--- js_CodeSegment.js
+++ js_CodeSegment.js
@@ -157,6 +157,8 @@ for(var i, type; type=['String', 'Array', 'Number'][i++];){
 	})(type)
 };
 
+
+
 Function.prototype.before=function(beforefn){
 
 	var _self=this;
@@ -179,5 +181,193 @@ Function.prototype.after=function(afterfn){
 
 		return ret;
 	}
-	
+
+}
+
+
+var monthlyCost=0;
+
+function cost(money){
+	monthlyCost+=money;
+}
+
+
+var cost=(function(){
+	var args=[];
+
+	retrun function(){
+		if(arguments.length===0){
+			var money=0;
+			for(var i=0, l=args.length; i<l; i++){
+				money += args[i];
+			}
+			return money;
+		}else{
+			[].push.apply(args, arguments)
+		}
+	}
+})();
+
+
+var currying=function(fn){
+
+	var args=[];
+	return function(){
+		if(arguments.length===0){
+			return fn.apply(this, args)
+		}else{
+			[].push.apply(args, arguments);
+			return arguments.callee;
+		}
+	}
+}
+
+var costl=(function(){
+	var money=0;
+	return function(){
+		for(var i=0, l=arguments.length; i<l; i++){
+
+			money+=arguments[i];
+
+		}
+		return money;
+	}
+})()
+
+
+Function.prototype.uncurrying=function(){
+	var _self=this;
+	return function(){
+		var obj=Array.prototype.shift.call(arguments);
+		return _self.apply(obj, arguments)
+	}
+}
+
+
+for(var i=0, fn, ary=['push', 'shift', 'forEach']; fn=ary[i++];){
+	Array[fn]=Array.prototype[fn].uncurrying();
+}
+
+//传统单例模式
+var CreateDiv=(function(){
+
+	var instance;
+	var CreateDiv=function(html){
+		if(instance) return instance;
+
+		this.html=html;
+		this.init();
+		return instance=this;
+	};
+
+	CreateDiv.prototype.init=function(){
+		var div=document.createElement('div');
+		div.innerHTML=this.html;
+		document.body.appendChild(div);
+	};
+
+	return CreateDiv;
+})();
+
+
+
+var CreateDivl=function(html){
+	this.html=html;
+	this.init();
+};
+
+
+CreateDivl.prototype.init=function(){
+	var div=document.createElement('div');
+	div.innerHTML=this.html;
+	document.body.appendChild(div);
+}
+//代理模式
+var ProxySingletonCreateDiv=(function(){
+
+	var instance;
+	return function(html){
+		if(!instance) instance=new CreateDivl(html);
+
+		return instance;
+		
+	}
+})();
+
+
+var MyApp={};
+
+MyApp.namespace=function(name){
+	var parts=name.split('.');
+	var current=MyApp;
+
+	for(var i in parts){
+		if(!current[parts[i]]){
+			current[parts[i]]={};
+		}
+
+		current=current[parts[i]];
+
+	}
+};
+
+
+var createLoginLayer=(function(){
+	var div;
+	return function(){
+		if(!div){
+			div=document.createElement('div');
+			div.innerHTML="this is LoginLayer ";
+			div.style.display="none";
+			document.body.appendChild(div);
+		}
+	}
+})();
+
+var getSingle=function(fn){
+	var result;
+	return function(){
+		/*if(!result){
+			result=fn.apply(this, arguments);
+		}*/
+
+		return result || (result=fn.apply(this, arguments));
+
+	}
+};
+
+var createLoginDiv=function(){
+	var div=document.createElement('div');
+	div.innerHTML='this is login layer';
+	div.style.display='none';
+	document.body.appendChild(div);
+	retrun div;
+}
+
+var createSingleLoginLayerDiv=getSingle(createLoginDiv);
+
+var createIframe=function(){
+	var iframe=document.createElement('iframe');
+	document.body.appendChild(iframe);
+	retrun iframe;
+}
+
+var createIframeLayer=getSingle(createIframe);
+
+
+//策略模式
+var performaceS=function(salary){
+	return salary*4;
+};
+var performaceA=function(salary){
+	return salary*3;
+};
+var performaceB=function(salary){
+	return salary*2;
+};
+
+var calculateBonus=function(performace, salary){
+	if(performace==='S') return performaceS(salary);
+	if(performace==='A') return performaceA(salary);
+	if(performace==='B') return performaceB(salary);
 }
