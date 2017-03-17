c86b9139a1a74903e8ca347b196d50e90ec5833b
zhangmr <zhangmr@zendaimoney.com>
2017-03-17 18:05:29 +0800

    change

--- js_CodeSegment.js
+++ js_CodeSegment.js
@@ -849,8 +849,314 @@ var events={
 	}
 };
 
+events.remove=function(key, fn){
+	var fns=this.clientList[key];
+
+	if(!fns) return false;
+
+	if(!fn){
+		fns && (fns.length=0);
+	}else{
+		for(var l=fns.length-1; l>=0; l--){
+			var _fn=fns[l];
+			if (_fn===fn) fns.splice(l, 1);
+		}
+	}
+}
+
+
 var installEvent=function(obj){
 	for(var i in events){
 		obj[i]=events[i];
 	}
+};
+
+
+var Event=(function(){
+	var clientList={},
+		listen,
+		trigger,
+		remove;
+
+	listen=function(key, fn){
+		if(!clientList[key]){
+			clientList[key]=[];
+		}
+		clientList[key].push(fn)
+	};
+
+
+	trigger=function(){
+		var key=Array.prototype.shift.call(arguments);
+
+		var fns=clientList[key];
+
+		if(!fns || fns.length===0) return false;
+
+		for(var i=0, fn; fn=fns[i++];){
+			fn.apply(this, arguments);
+		}
+	};
+
+
+	remove=function(key, fn){
+		var fns=clientList[key];
+
+		if(!fns) return false;
+
+		if(!fn){
+			fns && (fns.length=0);
+		}else{
+
+			for(var l=fns.length-1; l>=0; l--){
+				var _fn=fns[l];
+				if(_fn===fn){
+					fns.splice(l, 1)
+				}
+			}
+		}
+	};
+
+	return {
+		listen:listen,
+		trigger:trigger,
+		remove:remove
+	}
+})();
+
+var a=(function(){
+	var count=1;
+	var button=document.getElementById('count');
+
+	button.onclick=function(){
+		Event.trigger('add', count++);
+	}
+})();
+
+var b=(function(){
+	var div=document.getElementById('show');
+	Event.listen('add', function(count){ div.innerHTML=count})
+})();
+
+
+var Events=(function(){
+	var global=this,
+		Events,
+		_default='default';
+
+	Events=function(){
+		var _listen,
+			_trigger,
+			_remove,
+			_slice=Array.prototype.slice,
+			_shift=Array.prototype.shift,
+			_unshift=Array.prototype.unshift,
+			namespaceCache={},
+			_create,
+			find,
+			each=function(ary, fn){
+				var ret;
+				for(var i=0, l=ary.length; i<l; i++){
+					var n=ary[i];
+					ret=fn.call(n, i, n);
+				}
+				return ret;
+			};
+
+			_listen=function(key, fn, cache){
+					if(!cache[key]) cache[key]=[];
+					cache[key].push(fn);
+			};
+
+			_remove=function(key, cache, fn){
+				if(cache[key]){
+					if(fn){
+						for(var i=cache[key].length; i>=0; i--){
+							if(cache[key][i]===fn){
+								cache[key].splice(i, 1);
+							}
+						}
+					}else{
+						cache[key]=[];
+					}
+				}
+			};
+
+
+			_trigger=function(){
+				var cache=_shift.call(arguments),
+					key=_shift.call(arguments),
+					args=arguments,
+					_self=this,
+					ret,
+					stack=cache[key];
+
+				if(!stack || !stack.length) return;
+
+				return each(stack, function(){
+					return this.apply(_self, args)
+				});
+
+			}
+
+			_create=function(namespace){
+
+				var namespace=namespace || _default;
+				var cache={},
+				    offlineStack=[],
+				    ret={
+				    	listen:function(key, fn, last){
+				    		_listen(key, fn, cache);
+				    		if(offlineStack === null) return false;
+
+				    		if(last==='last'){
+				    			offlineStack.length && offlineStack.pop()();
+				    		}else{
+				    			each(offlineStack, function(){
+				    				this();
+				    			})
+				    		}
+				    		offlineStack=null;
+				    	},
+
+				    	one:function(key, fn, last){
+				    		_remove(key, cache);
+				    		this.listen(key, fn, last);
+				    	},
+
+				    	remove:function(key, fn){
+				    		_remove(key, cache, fn);
+				    	},
+
+				    	trigger:function(){
+				    		var fn, 
+				    			args,
+				    			_self=this;
+
+				    			_unshift.call(arguments, cache);
+
+				    			args=arguments;
+				    			fn=function(){
+				    				return _trigger.apply(_self, args);
+				    			};
+
+				    			if(offlineStack) return offlineStack.push(fn);
+
+				    			return fn();
+				    	}
+				    };
+
+				    return namespace ? (namespaceCache[namespace] ? namespaceCache[namespace] : namespaceCache[namespace]=ret) :ret;
+
+
+			};
+
+			return {
+				create:_create,
+				one:function(key, fn, last){
+					var event=this.create();
+					event.one(key, fn, last);
+				},
+
+				remove:function(key, fn){
+					var event=this.create();
+					event.remove(key, fn);
+				},
+
+				listen:function(key, fn, last){
+					var event=this.create();
+					event.listen(key, fn, last)
+				},
+
+				trigger:function(){
+					var event=this.create();
+					event.trigger.apply(this, arguments)
+				}
+			};
+
+	}();
+
+	return Events;
+})();
+
+
+// command 命令模式
+var setCommand=function(button, command){
+	button.onclick=function(){
+		command.execute();
+	}
 }
+
+
+var MenuBar={
+	refresh:function(){ console.log('刷新菜单');}
+};
+
+var SubMenu={
+	add:function(){
+		console.log('增加子菜单');
+	},
+
+	del:function(){
+		console.log('删除子菜单');
+	}
+};
+
+var RefreshMenuBarCommand=function(receiver){
+	this.receiver=receiver;
+};
+
+RefreshMenuBarCommand.prototype.execute=function(){
+	this.receiver.refresh();
+}
+
+var AddSubMenuCommand=function(receiver){
+	this.receiver=receiver;
+};
+
+AddSubMenuCommand.prototype.execute=function(){
+	this.receiver.add();
+};
+
+var DelSubMenuCommand=function(receiver){
+	this.receiver=receiver;
+}
+DelSubMenuCommand.prototype.execute=function(){
+	this.receiver.del()
+};
+
+var refreshMenuBarCommand=new RefreshMenuBarCommand(MenuBar);
+var addSubMenuCommand = new AddSubMenuCommand(subMenu);
+var delSubMenuCommand = new DelSubMenuCommand(subMenu);
+
+var button1=document.getElementById('button1');
+var button2=document.getElementById('button2');
+var button3=document.getElementById('button3');
+
+setCommand(button1, refreshMenuBarCommand);
+setCommand(button2, addSubMenuCommand);
+setCommand(button3, delSubMenuCommand);
+
+
+//JavaScript 命令模式
+
+var setCommands=function(button, func){
+	button.onclick=function(){
+		func();
+	}
+};
+
+var Menubar={
+	refresh:function(){
+		console.log('reload page');
+	}
+};
+
+var RefreshmenuBarCommand=function(receiver){
+	return function(){
+		receiver.refresh();
+	}
+};
+
+var refreshmenuBarCommand=RefreshmenuBarCommand(Menubar);
+setCommands(button1, refreshmenuBarCommand);
