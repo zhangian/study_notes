e0e37832e1d2e46a84198b68b2e8ee37a72a5d79
zhangmr <zhangmr@zendaimoney.com>
2017-05-08 18:02:58 +0800

    js

--- js_CodeSegment.js
+++ js_CodeSegment.js
@@ -1399,3 +1399,71 @@ File.prototype.remove=function(){
 	}
 	
 }
+
+
+//职责链模式
+
+var order500=function(orderType, pay, stock){
+
+	if(orderType ===1 && pay ===true){
+		console.log('500元定金预购， 得到100元优惠券');
+	}else{
+		return 'nextSuccessor'; //我不知道下一个节点是谁，反正把请求往后面传递
+	}
+
+};
+
+var order200=function(orderType, pay, stock){
+
+	if(orderType ===2 && pay===true){
+		console.log('200元定金预购，得到50元优惠券');
+	}else{
+		return 'nextSuccessor'; 
+	}
+
+};
+
+var orderNormal = function(orderType, pay, stock){
+
+	if(stock > 0){
+
+		console.log('普通购买，无优惠券');
+
+	}else{
+
+		console.log('手机库存不足');
+
+	}
+
+};
+
+var Chain=function(fn){
+	this.fn=fn;
+	this.successor=null;
+}
+
+Chain.prototype.setNextSuccessor=function(successor){
+	return this.successor = successor;
+}
+
+Chain.prototype.passRequest=function(){
+
+	var ret =this.fn.apply(this, arguments);
+
+	if(ret === 'nextSuccessor'){
+		return this.successor && this.successor.passRequest.apply(this.successor, arguments)
+	}
+	debugger;
+	return ret;
+}
+
+var chainOrder500 = new Chain(order500);
+var chainOrder200 = new Chain(order200);
+var chainNormal   = new Chain(orderNormal);
+
+chainOrder500.setNextSuccessor(chainOrder200);
+chainOrder200.setNextSuccessor(chainNormal);
+
+chainOrder500.passRequest(1, true, 500);
+chainOrder500.passRequest(2, true, 200);
+chainOrder500.passRequest(1, false, 0);
